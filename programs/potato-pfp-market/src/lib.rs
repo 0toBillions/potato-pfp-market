d2cc17d740ccb0e8c6f79f3b8a0bf4437cbcc8ef
bagsmaxi <bagsmaxi0@gmail.com>
2026-01-28 22:37:16 +0700

    Fix invalid program ID base58

--- programs/potato-pfp-market/src/lib.rs
+++ programs/potato-pfp-market/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Token, TokenAccount, Transfer};
 
-declare_id!("PoTaT0PFPMarket111111111111111111111111111");
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 pub mod state;
 pub mod errors;
